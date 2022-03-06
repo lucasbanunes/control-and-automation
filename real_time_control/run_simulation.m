51eb5b0f24ee2a823b5969424704ebf567dde28e
lucasbanunes <lucasbanunes@gmail.com>
2022-03-05 22:25:08 -0300

    Added simulations folder to have a more general script, implementing complete observers.

--- real_time_control/run_simulation.m
+++ real_time_control/run_simulation.m
@@ -7,31 +7,38 @@ h = 1; % Sample step
 h_plant = (1e-3); % Integration step
 
 
-% Continuous state space
+% Continuous plant state space (double integrator)
 A = [0 1; 0 0];
+n = length(A);    % System order
 B = [0; 1];
 C = [1 0];
-D = [0];
+D = 0;
+G = 0*eye(n);
+Q = 0*eye(n);   % process_noise_variance
+R = 0;   % measure_noise_variance
 
 % Initial plant states
 x0 = zeros(2, 1);
 x0(1) = 1;
 x0(2) = 0;
 
-% Initial observer states;
-x0_observer = zeros(2, 1);
-x0_observer(1) = 0;
-x0_observer(2) = 0;
-
 % Discrete model of double integrator
 Phi = [1 h; 0 1];    
 Gamma = [h^2/2; h];
 C_discrete = [1 0];
 D_discrete = 0;
-sysd = ss(Phi, Gamma, C, D, h);
+G_discrete = 0*eye(n);
+Q_discrete = G*Q*(G')*h;   %For h sufficiently small
+R_discrete = R*h;
+sysd = ss(Phi, Gamma, C_discrete, D_discrete, h);
 
-% System order 
-n = length(Phi);
+% Observer parameters;
+% L = lqe(Phi, G_discrete, C_discrete, Q_discrete, R_discrete); %For Kalman
+% L = 100*ones(n,1);
+L = place(Phi', C_discrete', [0.01;0.03]);
+x0_observer = zeros(2, 1);
+x0_observer(1) = 0;
+x0_observer(2) = 0;
 
 % Optimization horizon
 N = 20;
@@ -43,33 +50,21 @@ reference=0;
 Q1 = eye(n);
 Q2 = 1;
 Q0 = eye(n);
-Q12 = 0;    
+Q12 = 0;
 
-% Arbitray gains
-non_optimal_poles = [0.1 0.15];
-K_non_optimal = place(Phi, Gamma, non_optimal_poles);
-
-% LQR stationary gains
-[X,K,lqr_poles,info] = idare(Phi,Gamma,Q1,Q2,[],[]);
-observer_eig = [0.1 0.2];
-
-% Pertubations
-noise_seed = 42;
-process_noise_variance = 0.05;
-measure_noise_variance = 0.05;
-measure_noise_gain = 0;
-wind_initial_value = 0;
-wind_final_value = 0;
-wind_steptime = 5;
-
-%Kalman Filter params
-G = [h^2/2; h];
-Q = process_noise_variance;
-R = measure_noise_variance;
+%Control Gain computed for static LQR
+% [X,K,lqr_poles,info] = idare(Phi,Gamma,Q1,Q2,[],[]);
+[K, S, CLP] = lqr(sysd, Q1, Q2);
 
 % Input saturation
 sat_min = -(10000);
 sat_max = (10000);
+
+% Pertubations
+noise_seed = 42;
+wind_initial_value = 0;
+wind_final_value = 0;
+wind_steptime = 5;
 
 % out = sim('real_time_optimal_control.slx', 30);
 % 
